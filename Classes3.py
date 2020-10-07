76e2c3d3c3da7df0bde0e9a6462c01ac5b6aaf29
nathan-quick <nathan.quick@ucollege.edu>
2020-10-07 14:06:16 -0500

    Added some comments and adjusted the formating

--- Classes3.py
+++ Classes3.py
@@ -5,11 +5,13 @@ class Student:
         self.__LastName = LastName
         self.__CreditsAccomplished = CreditsAccomplished
         self.__GPA = GPA
-    
+
     def print_headers(self, headers):
         for h in headers:
             print(h)
-    
+
+    # stores all values in a dictionary for them to be called by value
+    # this way I did not need to make a new get for every value
     def get_student(self, value):
         stats = {
             'ID': self.__ID,
@@ -20,8 +22,9 @@ class Student:
         }
         return stats[value]
 
-s1 = Student(1123, 'Nathan', 'Quick', 17, 4.0)    
+
+s1 = Student(1123, 'Nathan', 'Quick', 17, 4.0)
 
 with open('CPTR226-HW25-Data.csv', newline='') as csvfile:
-    for l in csvfile:
-        print(l)
+    for line in csvfile:
+        print(line)
